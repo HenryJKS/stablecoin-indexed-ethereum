55ca0de36e4fe5a1396e7c9c807279882ab7a629
HenryJKS <henryjooji30@gmail.com>
2024-09-12 18:13:12 -0300

    test: init

--- test/HJKStableCoin.test.js
+++ test/HJKStableCoin.test.js
@@ -3,9 +3,15 @@ const HJKStableCoin = artifacts.require("HJKStableCoin");
 contract("HJK Stablecoin", (accounts) => {
   let tokenStablecoin;
 
-  const [owner, recipient] = accounts;
+  const [owner] = accounts;
 
   beforeEach(async () => {
     tokenStablecoin = await HJKStableCoin.deployed();
+  });
+
+  it("symbol name", async() => {
+    let symbol = await tokenStablecoin.symbol();
+    
+    assert.equal(symbol, 'HSC');
   });
 });
